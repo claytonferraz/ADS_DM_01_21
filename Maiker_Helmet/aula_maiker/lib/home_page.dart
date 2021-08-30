f0ce759ac0cd3c01db20680e3b25bf021c3b8d60
Maiker <maiker.brasil@gmail.com>
2021-08-30 14:30:11 -0400

    Aula 1.5.2

---
+++ Maiker_Helmet/aula_maiker/lib/home_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('App Aula Maiker'),
+      ),
+      body: Center(
+          child: GestureDetector(
+              child: Text(
+                'Click $counter',
+                style: TextStyle(fontSize: 50),
+              ),
+              onTap: () {
+                setState(() => counter--);
+              })),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() => counter++);
+        },
+        child: Icon(Icons.add_circle),
+      ),
+    );
+  }
+}
