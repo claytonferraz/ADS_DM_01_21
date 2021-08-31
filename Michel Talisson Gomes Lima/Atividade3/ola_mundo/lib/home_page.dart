491cfeceee531f4a9ebf0cdae932de5338370bfd
[MichelTalissonGomesLima] <[micheltalissongomeslima51@gmail.com]>
2021-08-30 22:16:31 -0400

    Aula 1.7.3 Switch

--- Michel Talisson Gomes Lima/Atividade3/ola_mundo/lib/home_page.dart
+++ Michel Talisson Gomes Lima/Atividade3/ola_mundo/lib/home_page.dart
@@ -9,23 +9,18 @@ class HomePage extends StatefulWidget {
 
 class HomePageState extends State<HomePage> {
   int counter = 0;
+  bool isDark = false;
   @override
   Widget build(BuildContext context) {
-    var container = Container(
-        height: 150,
-        width: 150,
-        color: Colors.red,
-        child: Center(
-          child: Container(
-            height: 50,
-            width: 50,
-            color: Colors.blue,
-            child: Icon(Icons.remove_circle_outline),
-          ),
-        ));
     return Scaffold(
       appBar: AppBar(title: Text('App ADS')),
-      body: container,
+      body: Center(
+          child: Switch(
+        value: isDark,
+        onChanged: (value) {
+          print(value);
+        },
+      )),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add_circle),
         onPressed: () {
